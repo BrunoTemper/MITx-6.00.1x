3dd363b6ebe3f60324d3b7afa1ae5e5dc41d0d9d
Amir Kurtovic <amirkurtovic@gmail.com>
2014-03-11 16:22:58 -0500

    Adding solution for getGuessedWord()

--- Week3/HangmanPart1.py
+++ Week3/HangmanPart1.py
@@ -5,8 +5,30 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-
     for l in range(len(secretWord)):
-                   if (secretWord[l] not in lettersGuessed):
-                       return False
+        if (secretWord[l] not in lettersGuessed):
+            return False
     return True
+
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    guessedSoFar = []
+    for l in range(len(secretWord)):
+        if (secretWord[l] in lettersGuessed):
+            guessedSoFar.append(secretWord[l])
+        else:
+            guessedSoFar.append("_ ")
+
+    guessedWord = ""
+    for char in range(len(guessedSoFar)):
+        guessedWord+=str(guessedSoFar[char])
+
+    return guessedWord
+            
+
+
