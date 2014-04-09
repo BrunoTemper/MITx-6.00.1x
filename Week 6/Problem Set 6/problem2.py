f3779288f2e79c94a715f26760df02dd09bfe63e
Amir Kurtovic <amirkurtovic@gmail.com>
2014-04-08 20:30:16 -0500

    Adding partial solution for Problem 2
    Does not work properly for all test cases (eg. no correct shift key
    found)

--- Week 6/Problem Set 6/problem2.py
+++ Week 6/Problem Set 6/problem2.py
@@ -4,8 +4,30 @@ def findBestShift(wordList, text):
 
     text: string
     returns: 0 <= int < 26
+
+
+    PSEUDO CODE:
+    Create a dictionary called scores
+    In a loop from 0 to 26:
+        Use applyShift to decrypted the encrypted text using the loop index
+        Split the encrypted text into a list
+        For reach word in the list:
+            If isWord() is true, increment score by one
+        Add score to the score dictionary with key of index
+    After going through the loop, return the dictionary key with the highest score value
     """
-    ### TODO
+    scores = {}
+    
+    for i in range(26):
+        score = 0
+        decrypted = applyShift(text, i)
+        decrypted.split(' ')
+        for w in decrypted:
+            if (isWord(wordList, w)):
+                score += 1
+        scores[i] = score
+
+    return max(scores, key=scores.get)
 
 def decryptStory():
     """
